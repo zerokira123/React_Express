030c0029ba6bb6d094f90bed96ae58570527624d
zerokira123 <thfication01@gmail.com>
2021-03-04 00:05:43 +0530

    finished  styling

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -26,6 +26,7 @@ const myElement = <MyComponent text="Hello, world!" />
 // Render it into the DOM
 ReactDOM.render(myElement, document.querySelector('#pp'))
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
